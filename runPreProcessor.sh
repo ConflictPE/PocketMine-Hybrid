95160ed729517f7989970f5f2d804c6724017db9
JackNoordhuis <daswaggamer@gmail.com>
2018-03-04 01:14:50 +1100

    Some improvements and fixes for preprocessor script

--- runPreProcessor.sh
+++ runPreProcessor.sh
@@ -23,8 +23,18 @@ echo Running preprocessor on \"$DIR\"...
 
 "$PHP_BINARY" tests/preprocessor/PreProcessor.php --path "$DIR"
 
+if [ $? -ne 0 ]; then
+	echo Failed to apply preprocessor macros!
+	exit 1
+fi
+
 echo Running code optimizer on \"$DIR\"...
 
 "$PHP_BINARY" tests/preprocessor/CodeOptimizer.php --path "$DIR"
 
-echo Code optimizer and preprocessor completed successfully.
+if [ $? -ne 0 ]; then
+	echo Failed to apply preprocessor code optimizations!
+	exit 1
+fi
+
+echo Code optimizer and preprocessor modifications applied successfully.
